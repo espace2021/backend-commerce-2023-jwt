95b13788d35923f6dc528b02b577b193ba245d89
espace2021 <92257192+espace2021@users.noreply.github.com>
2023-08-27 10:37:48 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -47,15 +47,5 @@ isActive: {
 {
     timestamps: true,
 },
-)
-userSchema.pre('save', async function(next) {
-
-    if (!this.isModified('password')) return next()
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(this.password, salt)
-    this.password = hashedPassword
-    next()
-
-})
-
-module.exports = mongoose.model('User', userSchema)
+);
+module.exports=mongoose.model('User',userSchema)
