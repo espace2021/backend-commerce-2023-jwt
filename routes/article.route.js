c014b2f066aec4d1242a51cd30a41c6c908d5512
espace2021 <92257192+espace2021@users.noreply.github.com>
2023-08-27 15:49:08 +0200

    Update article.route.js

--- routes/article.route.js
+++ routes/article.route.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Article=require("../models/article")
+//const {verifyToken} =require("../middleware/verif-token")
+//const { uploadFile } = require('../middleware/upload-file')
 
 // chercher un article par s/cat
 router.get('/scat/:scategorieID',async(req, res)=>{
@@ -25,68 +27,6 @@ router.get('/', async (req, res, )=> {
 
 });
 
-// afficher la liste des articles par page
-router.get('/productspage/', async(req, res) => {
-
-    const page = req.query.page || 1; // Get the current page number from the query parameters
-    const pagesize = req.query.pagesize ||5; // Number of items per page
-    
-    // Calculez le nombre d'éléments à sauter (offset)
-    const offset = (page - 1) * pagesize;
-    try {
-    // Effectuez la requête à votre source de données en utilisant les paramètres de pagination
-    const articles = await Article.find( {}, null, {sort: {'_id': -1}})
-      .skip(offset)
-      .limit(pagesize)
-     
-    
-      res.status(200).json(articles);
-  } catch (error) {
-      res.status(404).json({ message: error.message });
-  }
-  });
- 
-  // nombre total des enregistrements
-router.get('/nombreTot/', async (req, res, )=> {
-    try {
-        const articles = await Article.find().exec();
-                
-        res.status(200).json({tot:articles.length});
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-  
-  });
-  
-// 2 ème solution paginate avec filtre
-
-router.get('/filtres/', async(req, res) => {
-
-  const filtre = req.query.filtre || ""; 
-  const page = req.query.page || 1; // Get the current page number from the query parameters
-  const limit = req.query.limit ||5; // Number of items per page
-
-  // Calculez le nombre d'éléments à sauter (offset)
-  const offset = (page - 1) * limit;
-  try {
-  // Effectuez la requête à votre source de données en utilisant les paramètres de pagination
-  // $option is used to make the search case insensitive.
-  const articles = await Article.find({ designation: { $regex: filtre, $options: "i" }}, null, {sort: {'_id': -1}})
-    .skip(offset)
-    .limit(limit)
-    .populate("scategorieID").exec()
-    
-
-  const articlesNb = await Article.find({ designation: { $regex: filtre, $options: "i" }})
-   .exec()
-
-    res.status(200).json({articles: articles, longueur : articlesNb.length});
-} catch (error) {
-    res.status(404).json({ message: error.message });
-}
-});
-
-
 
 // créer un nouvel article
 router.post('/', async (req, res) =>  { 
@@ -95,7 +35,6 @@ router.post('/', async (req, res) =>  {
     try {
         const response =await nouvarticle.save();
         const articles = await Article.findById(response._id).populate("scategorieID").exec();
-        console.log(articles)
         res.status(200).json(articles);
     } catch (error) {
         res.status(404).json({ message: error.message });
@@ -124,7 +63,6 @@ router.put('/:articleId', async (req, res)=> {
       { new: true }
     );
     const articles = await Article.findById(art._id).populate("scategorieID").exec();
-    console.log(articles)
     res.status(200).json(articles);
     } catch (error) {
     res.status(404).json({ message: error.message });
@@ -141,8 +79,4 @@ router.delete('/:articleId', async (req, res)=> {
     res.status(404).json({ message: error.message });
     }   
 });
-
-
-
-
 module.exports = router;
